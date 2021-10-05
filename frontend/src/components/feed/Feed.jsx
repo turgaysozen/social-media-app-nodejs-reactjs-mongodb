0ca87b4cd7d25403abaa7d83f7ffd43bb127823d
turgaysozen <turgaysozen@hotmail.com>
2021-10-05 18:47:17 +0300

    implement data logic for home page

--- frontend/src/components/feed/Feed.jsx
+++ frontend/src/components/feed/Feed.jsx
@@ -1,26 +1,16 @@
 import './feed.css'
 import Share from '../share/Share'
 import Post from '../post/Post'
+import { Posts } from '../../fakeDate'
 
 export default function Feed() {
     return (
         <div className='feed'>
             <div className="feedWrapper">
-                <Share/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-
+                <Share />
+                {Posts.map(post => (
+                    <Post key={post.id} post={post} />
+                ))}
             </div>
         </div>
     )
--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -1,15 +1,16 @@
 import './post.css'
 import { MoreVert } from '@mui/icons-material'
+import { Users } from '../../fakeDate'
 
-export default function Post() {
+export default function Post({post}) {
     return (
         <div className='post'>
             <div className="postWrapper">
                 <div className="postTop">
                     <div className="postTopLeft">
-                        <img src="/assets/person/1.jpeg" alt="" className="postProfileImg" />
-                        <span className="postUserName">Turgay SÖZEN</span>
-                        <span className="postDate">5 min. ago</span>
+                        <img src={Users.filter(u => u.id === post.userId)[0].profilePicture} alt="" className="postProfileImg" />
+                        <span className="postUserName">{Users.filter(u => u.id === post.userId)[0].username}</span>
+                        <span className="postDate">{post.date}</span>
                     </div>
                     <div className="postgTopRight">
                         <MoreVert/>
@@ -17,17 +18,17 @@ export default function Post() {
                 </div>
                 <hr className="postHr" />
                 <div className="postCenter">
-                    <span className="postText">Post Text</span>
-                    <img src="/assets/post/1.jpeg" alt="" className="postImg" />
+                    <span className="postText">{post.desc}</span>
+                    <img src={post.photo} alt="" className="postImg" />
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
                         <img src="assets/like.png" alt="" className="likeIcon" />
                         <img src="assets/heart.png" alt="" className="likeIcon" />
-                        <span className="likeCount">45 people like it</span>
+                        <span className="likeCount">{post.like} people like it</span>
                     </div>
                     <div className="postBottomRight">
-                        <span className="postCommentText">11 comments</span>
+                        <span className="postCommentText">{post.comment} comments</span>
                     </div>
                 </div>
             </div>
