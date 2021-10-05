53e4d7da0359e28b6371056f9df91ce3753f4bd4
turgaysozen <turgaysozen@hotmail.com>
2021-10-05 18:28:58 +0300

    change style of home page

--- frontend/src/components/rightbar/Rightbar.jsx
+++ frontend/src/components/rightbar/Rightbar.jsx
@@ -36,6 +36,105 @@ export default function Rightbar() {
                     </li>
                 </ul>
             </div>
+            <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <img src="assets/gift.png" alt="" className="birthdayImg" />
+                    <div className="birthdayText">
+                        <b>Turgay</b> and <b>3 others</b> have birthday today!
+                    </div>
+                </div>
+                <img src="assets/ad.png" alt="" className="rightbarAds" />
+                <h3 className="rightbarTitle">Online Friends</h3>
+                <ul className="rightbarFriendList">
+                    <li className="rightbarFriend">
+                        <div className="rightbarImgContainer">
+                            <img src="assets/person/3.jpeg" alt="" className="rightbarFriendImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarFriendName">Turgay SÖZEN</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarImgContainer">
+                            <img src="assets/person/4.jpeg" alt="" className="rightbarFriendImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarFriendName">Burak Gül</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarImgContainer">
+                            <img src="assets/person/5.jpeg" alt="" className="rightbarFriendImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarFriendName">Tyler Durden</span>
+                    </li>
+                </ul>
+            </div>
+            <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <img src="assets/gift.png" alt="" className="birthdayImg" />
+                    <div className="birthdayText">
+                        <b>Turgay</b> and <b>3 others</b> have birthday today!
+                    </div>
+                </div>
+                <img src="assets/ad.png" alt="" className="rightbarAds" />
+                <h3 className="rightbarTitle">Online Friends</h3>
+                <ul className="rightbarFriendList">
+                    <li className="rightbarFriend">
+                        <div className="rightbarImgContainer">
+                            <img src="assets/person/3.jpeg" alt="" className="rightbarFriendImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarFriendName">Turgay SÖZEN</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarImgContainer">
+                            <img src="assets/person/4.jpeg" alt="" className="rightbarFriendImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarFriendName">Burak Gül</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarImgContainer">
+                            <img src="assets/person/5.jpeg" alt="" className="rightbarFriendImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarFriendName">Tyler Durden</span>
+                    </li>
+                </ul>
+            </div>
+            <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <img src="assets/gift.png" alt="" className="birthdayImg" />
+                    <div className="birthdayText">
+                        <b>Turgay</b> and <b>3 others</b> have birthday today!
+                    </div>
+                </div>
+                <img src="assets/ad.png" alt="" className="rightbarAds" />
+                <h3 className="rightbarTitle">Online Friends</h3>
+                <ul className="rightbarFriendList">
+                    <li className="rightbarFriend">
+                        <div className="rightbarImgContainer">
+                            <img src="assets/person/3.jpeg" alt="" className="rightbarFriendImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarFriendName">Turgay SÖZEN</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarImgContainer">
+                            <img src="assets/person/4.jpeg" alt="" className="rightbarFriendImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarFriendName">Burak Gül</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarImgContainer">
+                            <img src="assets/person/5.jpeg" alt="" className="rightbarFriendImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarFriendName">Tyler Durden</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
