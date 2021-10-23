31051a25ad89387040ca137a542c343ae7abaf63
turgaysozen <turgaysozen@hotmail.com>
2021-10-24 01:48:07 +0300

    Add axios and fetch data for Feed

--- frontend/src/components/feed/Feed.jsx
+++ frontend/src/components/feed/Feed.jsx
@@ -1,15 +1,26 @@
 import './feed.css'
 import Share from '../share/Share'
 import Post from '../post/Post'
-import { Posts } from '../../fakeData'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 export default function Feed() {
+    const [post, setPost] = useState([])
+
+    useEffect(() => {
+        (async () => {
+            const posts = await axios.get('http://localhost:8800/api/posts/timeline/60cc36dab620653738c9f78f')
+            setPost(posts.data)
+        })
+            ()
+    }, [])
+
     return (
         <div className='feed'>
             <div className="feedWrapper">
                 <Share />
-                {Posts.map(post => (
-                    <Post key={post.id} post={post} />
+                {post.map(post => (
+                    <Post key={post._id} post={post} />
                 ))}
             </div>
         </div>
