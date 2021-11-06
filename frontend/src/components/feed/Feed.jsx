9c228680b19922c0ece161bb17ee521c139cf0e5
turgaysozen <turgaysozen@hotmail.com>
2021-11-06 19:59:58 +0300

    Fetch current user followings post

--- frontend/src/components/feed/Feed.jsx
+++ frontend/src/components/feed/Feed.jsx
@@ -2,20 +2,22 @@ import './feed.css'
 import Share from '../share/Share'
 import Post from '../post/Post'
 import axios from 'axios'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useContext } from 'react'
+import { AuthContext } from '../../context/AuthContext'
 
 export default function Feed({ username }) {
     const [post, setPost] = useState([])
+    const { user } = useContext(AuthContext)
 
     useEffect(() => {
         (async () => {
             const posts = username
                 ? await axios.get(`http://localhost:8800/api/posts/profile/${username}`)
-                : await axios.get('http://localhost:8800/api/posts/timeline/60cc36dab620653738c9f78f')
+                : await axios.get('http://localhost:8800/api/posts/timeline/' + user._id)
             setPost(posts.data)
         })
             ()
-    }, [username])
+    }, [username, user._id])
 
     return (
         <div className='feed'>
