f592d8532a79cbf7b788135d8661d1672adf8079
turgaysozen <turgaysozen@hotmail.com>
2021-11-21 21:45:44 +0300

    Upload files

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const dotenv = require('dotenv')
 const helmet = require('helmet')
 const morgan = require('morgan')
 const cors = require('cors')
+const multer = require('multer')
+const path = require('path')
 
 const userRoute = require('./routes/users')
 const authRoute = require('./routes/auth')
@@ -18,12 +20,32 @@ connection.once("open", function () {
     console.log("MongoDB database connection established successfully");
 });
 
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+
 // middleweare
 app.use(express.json())
 app.use(helmet())
 app.use(morgan('common'))
 
-app.use(function(req, res, next) {
+const storage = multer.diskStorage({
+    destination: (req, res, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name)
+    }
+})
+
+const upload = multer({ storage })
+app.post('/api/upload', upload.single('file'), (req, res) => {
+    try {
+        return res.status(200).json('file uploaded successfully')
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
